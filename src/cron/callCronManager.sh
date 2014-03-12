113711ad64fe2e3d06a56fef48e24ffee3da4002
Richard Gillette <richardgillette311@gmail.com>
2014-03-12 08:47:53 -0400

    Delete callCronManager.sh

--- src/cron/callCronManager.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#this script will be called by the cron every 5 minutes which 
-#will in turn call the actual poller to go get new information
-
-.  /home/aaron/repo/socialreader/src/cron/socialConfig.txt
-eval /usr/bin/wget -q -O /dev/null http://'$HOST'/cron/poller/cronManager.php?index='$INDEX'&mapping='$MAPPING'&host='$HOST'&port='$PORT'
-
