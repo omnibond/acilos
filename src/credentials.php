abd47be953ed730375edca87d200ab09bcaebd4d
Aaron <aaron@parallels-Parallels-Virtual-Platform.(none)>
2014-03-13 16:42:26 -0400

    css changes on credentials.php

--- src/credentials.php
+++ src/credentials.php
@@ -56,6 +56,7 @@
 			"dojo/_base/window",
 			'dojo/dom-construct',
 			'dojo/topic',
+			'dojo/has',
 			'dojo/dom-style',
 			'dojo/_base/kernel',
 			"dojo/_base/lang",
@@ -79,6 +80,7 @@
 			domWindow,
 			domConstruct, 
 			topic, 
+			has,
 			domStyle,
 			kernel, 
 			lang, 
@@ -105,23 +107,29 @@
 					'<?php echo $tCount; ?>' == 0
 				){
 					var leftPane = new Container({
+						style: "text-align: center"
 					});
 					domWindow.body().appendChild(leftPane.domNode);
 					
 					var item = new ListItem({
-						label: "Welcome to FeedRider",
-						style: "border:none;height:35px;font-size;font-family:arial;font-size:20px"
-					})
+						label: "Welcome to",
+						style: "border:none;height:35px;font-size;font-family:arial;font-size:20px; text-align: center"
+					});
 					leftPane.addChild(item);
-					var item = new ListItem({
+					/*var item = new ListItem({
 						label: "Please enter social media app accounts",
 						style: "border:none;height:35px;font-size;font-family:arial;font-size:20px"
-					})
-					leftPane.addChild(item);
+					});*/
+
+					var acilosLoginDiv = domConstruct.create("div", {innerHTML: "<img src=app/resources/img/acilosLoginLogo.png>", style: "width: 304px; margin-left: auto; margin-right: auto; margin-top: auto; margin-bottom: auto"});
+					leftPane.domNode.appendChild(acilosLoginDiv);
+
+					var item = domConstruct.create("div", {innerHTML: "Please enter social media app accounts", style: "border:none;height:35px;font-size;font-family:arial;font-size:20px; text-align: center; height: auto"});
+					leftPane.domNode.appendChild(item);
 					var errorItem = new ListItem({
 						label: "",
 						style: "border:none;height:35px;font-size;font-family:arial;font-size:20px"
-					})
+					});
 					leftPane.addChild(errorItem);
 					
 					buildPage(leftPane, errorItem);
@@ -131,57 +139,69 @@
 			},
 			
 			buildPage = function(leftPane, errorItem){
+				domWindow.body().style.overflow = "scroll";
+
 				var list = new RoundRectList({
 					style: "border:none"
 				});
 				
 				//--------------------------------------------------------------------------------
 				var item = new ListItem({
-					style: "height:auto;border-left:none;right:none"
+					"class": "credentialListItem"
 				});
-				var instaKey = new TextBox({
-					placeHolder: "App Id/Key"
-				})
-				var instaSecret = new TextBox({
-					placeHolder: "App Secret"
-				})
-				var instaRedirect = new TextBox({
-					value: "http://" + '<?php echo $redirect; ?>' + "instaAccess.php",
-					disabled: true
-				})
-				var instaLogoDiv = domConstruct.create("span", {style: "margin-top:25px;float:left;width:100px;height:100px", "class":"loginLogo", innerHTML: "<img src=app/resources/img/instagramLogin.png>"});
+				var twitterKey = new TextBox({
+					placeHolder: "App Id/Key",
+					"class": "credentialTextBox"
+				});
+				var twitterSecret = new TextBox({
+					placeHolder: "App Secret",
+					"class": "credentialTextBox"
+				});
+				var twitterRedirect = new TextBox({
+					value: "http://" + '<?php echo $redirect; ?>' + "twitterAccess.php",
+					disabled: true,
+					"class": "credentialTextBox"
+				});
+				var twitterLogoDiv = domConstruct.create("span", {style: "margin-top:25px;float:left;width:100px;height:100px", "class":"loginLogo", innerHTML: "<img src=app/resources/img/twitterLogin.png>"});
 				var textBoxDiv = domConstruct.create("span", {style:"float:left"});
 				var holderDiv = domConstruct.create("div", {});
-				holderDiv.appendChild(instaKey.domNode);
+				holderDiv.appendChild(twitterKey.domNode);
 				textBoxDiv.appendChild(holderDiv);
 				
 				holderDiv = domConstruct.create("div", {});
-				holderDiv.appendChild(instaSecret.domNode);
+				holderDiv.appendChild(twitterSecret.domNode);
 				textBoxDiv.appendChild(holderDiv);
 				
 				holderDiv = domConstruct.create("div", {});
-				holderDiv.appendChild(instaRedirect.domNode);
+				holderDiv.appendChild(twitterRedirect.domNode);
 				textBoxDiv.appendChild(holderDiv);
 				
-				item.domNode.appendChild(instaLogoDiv);
+				item.domNode.appendChild(twitterLogoDiv);
 				item.domNode.appendChild(textBoxDiv);
+
+				var containerDiv = domConstruct.create("div", {});
+
+				containerDiv.appendChild(item.domNode);
 				
-				list.addChild(item);
+				list.domNode.appendChild(containerDiv);
 				
 				//--------------------------------------------------------------------------------
 				var item = new ListItem({
-					style: "height:auto;border-left:none;right:none"
+					"class": "credentialListItem"
 				});				
 				var faceKey = new TextBox({
-					placeHolder: "App Id/Key"
-				})
+					placeHolder: "App Id/Key",
+					"class": "credentialTextBox"
+				});
 				var faceSecret = new TextBox({
-					placeHolder: "App Secret"
-				})
+					placeHolder: "App Secret",
+					"class": "credentialTextBox"
+				});
 				var faceRedirect = new TextBox({
 					value: "http://" + '<?php echo $redirect; ?>' + "facebookAccess.php",
-					disabled: true
-				})
+					disabled: true,
+					"class": "credentialTextBox"
+				});
 				var faceLogoDiv = domConstruct.create("span", {style: "margin-top:25px;float:left;width:100px;height:100px", "class":"loginLogo", innerHTML: "<img src=app/resources/img/facebookLogin.png>"});
 				var textBoxDiv = domConstruct.create("span", {style:"float:left"});
 				var holderDiv = domConstruct.create("div", {});
@@ -199,55 +219,69 @@
 				item.domNode.appendChild(faceLogoDiv);
 				item.domNode.appendChild(textBoxDiv);
 				
-				list.addChild(item);
+				var containerDiv = domConstruct.create("div", {});
+
+				containerDiv.appendChild(item.domNode);
+				
+				list.domNode.appendChild(containerDiv);
 				
 				//--------------------------------------------------------------------------------
 				var item = new ListItem({
-					style: "height:auto;border-left:none;right:none"
+					"class": "credentialListItem"
 				});
-				var twitterKey = new TextBox({
-					placeHolder: "App Id/Key"
-				})
-				var twitterSecret = new TextBox({
-					placeHolder: "App Secret"
-				})
-				var twitterRedirect = new TextBox({
-					value: "http://" + '<?php echo $redirect; ?>' + "twitterAccess.php",
-					disabled: true
-				})
-				var twitterLogoDiv = domConstruct.create("span", {style: "margin-top:25px;float:left;width:100px;height:100px", "class":"loginLogo", innerHTML: "<img src=app/resources/img/twitterLogin.png>"});
+				var instaKey = new TextBox({
+					placeHolder: "App Id/Key",
+					"class": "credentialTextBox"
+				});
+				var instaSecret = new TextBox({
+					placeHolder: "App Secret",
+					"class": "credentialTextBox"
+				});
+				var instaRedirect = new TextBox({
+					value: "http://" + '<?php echo $redirect; ?>' + "instaAccess.php",
+					disabled: true,
+					"class": "credentialTextBox"
+				});
+				var instaLogoDiv = domConstruct.create("span", {style: "margin-top:25px;float:left;width:100px;height:100px", "class":"loginLogo", innerHTML: "<img src=app/resources/img/instagramLogin.png>"});
 				var textBoxDiv = domConstruct.create("span", {style:"float:left"});
 				var holderDiv = domConstruct.create("div", {});
-				holderDiv.appendChild(twitterKey.domNode);
+				holderDiv.appendChild(instaKey.domNode);
 				textBoxDiv.appendChild(holderDiv);
 				
 				holderDiv = domConstruct.create("div", {});
-				holderDiv.appendChild(twitterSecret.domNode);
+				holderDiv.appendChild(instaSecret.domNode);
 				textBoxDiv.appendChild(holderDiv);
 				
 				holderDiv = domConstruct.create("div", {});
-				holderDiv.appendChild(twitterRedirect.domNode);
+				holderDiv.appendChild(instaRedirect.domNode);
 				textBoxDiv.appendChild(holderDiv);
 				
-				item.domNode.appendChild(twitterLogoDiv);
+				item.domNode.appendChild(instaLogoDiv);
 				item.domNode.appendChild(textBoxDiv);
 				
-				list.addChild(item);
+				var containerDiv = domConstruct.create("div", {});
+
+				containerDiv.appendChild(item.domNode);
+				
+				list.domNode.appendChild(containerDiv);
 				
 				//--------------------------------------------------------------------------------
 				var item = new ListItem({
-					style: "height:auto;border-left:none;right:none"
+					"class": "credentialListItem"
 				});
 				var linkedKey = new TextBox({
-					placeHolder: "App Id/Key"
-				})
+					placeHolder: "App Id/Key",
+					"class": "credentialTextBox"
+				});
 				var linkedSecret = new TextBox({
-					placeHolder: "App Secret"
-				})
+					placeHolder: "App Secret",
+					"class": "credentialTextBox"
+				});
 				var linkedRedirect = new TextBox({
 					value: "http://" + '<?php echo $redirect; ?>' + "linkedinAccess.php",
-					disabled: true
-				})
+					disabled: true,
+					"class": "credentialTextBox"
+				});
 				var linkedLogoDiv = domConstruct.create("span", {style: "margin-top:25px;float:left;width:100px;height:100px", "class":"loginLogo", innerHTML: "<img src=app/resources/img/linkedinLogin.png>"});
 				var textBoxDiv = domConstruct.create("span", {style:"float:left"});
 				var holderDiv = domConstruct.create("div", {});
@@ -265,11 +299,17 @@
 				item.domNode.appendChild(linkedLogoDiv);
 				item.domNode.appendChild(textBoxDiv);
 				
-				list.addChild(item);
+				var containerDiv = domConstruct.create("div", {});
+
+				containerDiv.appendChild(item.domNode);
+				
+				list.domNode.appendChild(containerDiv);
+
 				leftPane.addChild(list);
 				
 				var save = new Button({
 					label: "Save",
+					style: "width: 60px; margin-left: auto; margin-right: auto",
 					onClick: lang.hitch(null, function(){
 						errorItem.set("label", "");
 						if(	(faceKey.get("value") != "" && faceSecret.get("value") == "" ||
