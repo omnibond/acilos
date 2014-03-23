2368741a3ed99a61224018d72cd87cf0fe68e6f1
Richard Gillette <richardgillette311@gmail.com>
2014-03-23 10:10:11 -0400

    Update amazonRebootManager.php

--- src/cron/poller/amazonRebootManager.php
+++ src/cron/poller/amazonRebootManager.php
@@ -32,8 +32,6 @@ use \Aws\Ec2\Ec2Client;
 $id = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
 
 $credArr = array(
-	'key'    => 'AKIAJS2QEUMYI7DKZYUA',
-	'secret' => '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',
 	'region' => 'us-east-1'
 );
 //create the client
