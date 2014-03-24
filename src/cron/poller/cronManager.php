7275faa7453700cd3358f7a71ec48919f63df826
root <root@ubuntu.(none)>
2014-03-24 16:28:30 -0400

    so many fixes before final build

--- src/cron/poller/cronManager.php
+++ src/cron/poller/cronManager.php
@@ -509,7 +509,7 @@ function getDiscussionObjects(){
 		$groupArr = array();
 		$counter = 0;
 		for($z = 0; $z < count($user['values']); $z++){
-			$groupPost = linkedInFetch('GET', '/v1/groups/'.$user['values'][$z]['group']['id'].'/posts:(creator:(first-name,last-name,picture-url,id,headline),title,summary,creation-timestamp,id,likes,comments,attachment:(image-url,content-domain,content-url,title,summary))', $token);
+			$groupPost = linkedInFetch('GET', '/v1/groups/'.$user['values'][$z]['group']['id'].'/posts:(creator:(first-name,last-name,picture-url,id,headline),title,summary,creation-timestamp,id,likes,comments,attachment:(image-url,content-domain,content-url,title,summary))', $obj['accessToken']);
 			$thing = objectToArray($groupPost);
 
 			for($t = 0; $t < count($thing['values']); $t++){
